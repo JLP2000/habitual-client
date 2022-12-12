2aa2a1f890edab4deda2cc96f4ddcf8e7c1f15a2
“liambrockpy” <liambrock72@gmail.com>
2022-12-12 17:48:06 +0000

    Add login/register pages

---
+++ client/assets/scripts/login.js
@@ -0,0 +1,35 @@
+const warning = document.getElementById("error-warning")
+
+document.getElementById("login-form").addEventListener("submit", async (e) => {
+	e.preventDefault()
+
+	const form = new FormData(e.target)
+
+	const options = {
+		method: "POST",
+		headers: {
+			Accept: "application/json",
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify({
+			username: form.get("username"),
+			password: form.get("password"),
+		}),
+	}
+	const response = await fetch("http://localhost:3000/users/login", options)
+
+	if (response.status == 200) {
+		// Read the data from the response
+		const data = await response.json()
+		console.log(data)
+
+		// Stash the token
+		localStorage.setItem("session", data.session)
+
+		// Send the user to login.html
+		window.location.assign("board.html")
+	} else {
+		warning.style.display = "block"
+	}
+	console.log(options)
+})
---
+++ client/assets/scripts/register.js
@@ -0,0 +1,32 @@
+const warning = document.getElementById("error-warning")
+
+document
+	.getElementById("register-form")
+	.addEventListener("submit", async (e) => {
+		e.preventDefault()
+
+		const form = new FormData(e.target)
+
+		const options = {
+			method: "POST",
+			headers: {
+				Accept: "application/json",
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify({
+				username: form.get("username"),
+				password: form.get("password"),
+			}),
+		}
+
+		const response = await fetch(
+			"http://localhost:3000/users/register",
+			options
+		)
+		if (response.status == 201) {
+			// Send the user to login.html
+			window.location.assign("login.html")
+		} else {
+			warning.style.display = "block"
+		}
+	})
