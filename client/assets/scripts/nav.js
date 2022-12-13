e0ebe38d55964151ac98d6199e46e7f78c5702d6
“liambrockpy” <liambrock72@gmail.com>
2022-12-13 16:08:32 +0000

    Add logout method

--- client/assets/scripts/nav.js
+++ client/assets/scripts/nav.js
@@ -4,3 +4,23 @@ let content = document.querySelector(".content")
 // 	sideBar.classList.toggle("collapse")
 // 	content.classList.toggle("collapse")
 // }
+
+const logoutBtn = document.getElementsByClassName("account-item")[0]
+
+logoutBtn.addEventListener("click", async (e) => {
+	e.preventDefault()
+	const options = {
+		method: "DELETE",
+		headers: {
+			Accept: "application/json",
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify({
+			sessionToken: localStorage.getItem("session"),
+		}),
+	}
+	const response = await fetch("http://localhost:3000/users/logout", options)
+
+	localStorage.removeItem("session")
+	window.location.assign("login.html")
+})
