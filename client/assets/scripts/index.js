35ef447fab8fb7f91cfc1e83056ce0b9f4ae16ad
“liambrockpy” <liambrock72@gmail.com>
2022-12-13 16:07:52 +0000

    Update functions

--- client/assets/scripts/index.js
+++ client/assets/scripts/index.js
@@ -2,36 +2,44 @@ const container = document.querySelector(".container")
 const editButton = document.querySelector("#editHabit")
 const deleteButton = document.querySelector("#deleteHabit")
 
-window.addEventListener("hashchange", updateLists)
+window.addEventListener("onload", renderLists)
 
-function updateLists() {
+async function renderLists() {
+	const overdueData = await getOverdue()
+	const todayData = await getToday()
+	const upcomingData = await upcomingData()
+
 	const overdue = document.getElementsByClassName("overdue-list")
-	loadList(overdue, overdueData)
+	overdueData.forEach((habitdate) => loadList(overdue, habitdate))
+
 	const upcoming = document.getElementsByClassName("upcoming-list")
-	loadList(upcoming, upcomingData)
-	if (todayData.length==0){
+	upcomingData.forEach((habitdate) => loadList(upcoming, habitdate))
+
+	if (todayData.length == 0) {
 		document.getElementsByClassName("bird").style.display = "none"
+
 		const today = document.getElementsByClassName("today-list")
-		loadList(today, todayData)
+		todayData.forEach((habitdate) => loadList(today, habitdate))
+	} else {
+		document.getElementsByClassName("bird").style.display = "inline-block"
 	}
 }
 
-
-function loadList(list,data) {
+function loadList(list, data) {
 	const checkbox = document.createElement("input")
 	checkbox.setAttribute("type", "checkbox")
-	checkbox.setAttribute("id", data.habit_id)
+	checkbox.setAttribute("id", data.habitdate_id)
 	checkbox.setAttribute("class", "input-checkbox")
 	const label = document.createElement("label")
 	label.setAttribute("class", "checkbox")
-	label.setAttribute("for", data.habit_id)
+	label.setAttribute("for", data.habitdate_id)
 	const span1 = document.createElement("span")
 	const svg = document.createElement("svg")
 	svg.setAttribute("viewport", "0 0 12 9")
 	const polyline = createElement("polyline")
 	polyline.setAttribute("points", "1 5 4 8 11 1")
 	const span2 = document.createElement("span")
-	span2.textContent = data.habit
+	span2.textContent = data.habitdate_id
 
 	list.appendChild(checkbox)
 	list.appendChild(label)
@@ -40,3 +48,8 @@ function loadList(list,data) {
 	svg.appendChild(polyline)
 	label.appendChild(span2)
 }
+
+const checkboxes = document.querySelectorAll("input[type=checkbox]")
+checkboxes.forEach((checkbox) => {
+	checkbox.addEventListener("change", updateCompleted)
+})
