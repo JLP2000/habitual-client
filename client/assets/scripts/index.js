54623ca4e4aa28eabccaadcb688578310b925ad9
“liambrockpy” <liambrock72@gmail.com>
2022-12-14 14:08:36 +0000

    Update habit sections

--- client/assets/scripts/index.js
+++ client/assets/scripts/index.js
@@ -16,13 +16,13 @@ async function renderToday() {
 	if (incomplete.length > 0) {
 		document.getElementById("noHabits").style.display = "none"
 		document.getElementById("bird").style.display = "none"
-	
-		const today = document.getElementsByClassName("today-list")
+
 		incomplete.forEach((habitdate) => loadList(today, habitdate))
 	} else {
 		document.getElementById("noHabits").style.display = "inline-block"
 		document.getElementById("bird").style.display = "inline-block"
-	}}
+	}
+}
 
 async function renderUpcoming() {
 	let incomplete = await getUpcoming()
@@ -30,30 +30,32 @@ async function renderUpcoming() {
 }
 
 function loadList(list, data) {
+	const form = document.createElement("form")
+	form.setAttribute("class", "habitdate")
 	const checkbox = document.createElement("input")
 	checkbox.setAttribute("type", "checkbox")
 	checkbox.setAttribute("id", data.habitdate_id)
 	checkbox.setAttribute("class", "input-checkbox")
+	checkbox.setAttribute("name", "completed")
 	const label = document.createElement("label")
 	label.setAttribute("class", "checkbox")
 	label.setAttribute("for", data.habitdate_id)
 	const span1 = document.createElement("span")
-	const svg = document.createElement("svg")
+	const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg")
 	svg.setAttribute("viewport", "0 0 12 9")
-	const polyline = document.createElementNS("http://www.w3.org/2000/svg", "polyline");
+	const polyline = document.createElementNS(
+		"http://www.w3.org/2000/svg",
+		"polyline"
+	)
 	polyline.setAttribute("points", "1 5 4 8 11 1")
 	const span2 = document.createElement("span")
 	span2.textContent = data.name
 
-	list.appendChild(checkbox)
-	list.appendChild(label)
+	list.appendChild(form)
+	form.appendChild(checkbox)
+	form.appendChild(label)
 	label.appendChild(span1)
 	span1.appendChild(svg)
 	svg.appendChild(polyline)
 	label.appendChild(span2)
 }
-
-const checkboxes = document.querySelectorAll("input[type=checkbox]")
-checkboxes.forEach((checkbox) => {
-	checkbox.addEventListener("change", updateCompleted)
-})
